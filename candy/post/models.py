da6073cb43bc6661a04f124c0814a418a9088df0
gardening-y <60008786@naver.com>
2023-11-08 22:18:11 +0900

    Feat: post 앱 구현

--- candy/post/models.py
+++ candy/post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Posting(models.Model):
+    title=models.CharField('Title', max_length=80)
+    content = models.TextField('Content')
+    upload_time = models.DateTimeField('Upload Time')
+
+    def __str__(self):
+        return self.title
