94281d01d070ccb050d3abf4c81273e848310489
gardening-y <60008786@naver.com>
2023-11-09 20:00:00 +0900

    Refactor: uri 장고 템플릿 문법으로 변경

--- candy/user/urls.py
+++ candy/user/urls.py
@@ -5,6 +5,6 @@ app_name = "user"
 
 urlpatterns = [
     path("login/", oauth_login, name='login'),
-    path("oauth/", Kakao.as_view()),
+    path("oauth/", Kakao.as_view(), name='oauth'),
     path("oauth/callback/", KakaoCallback.as_view()),
 ]
