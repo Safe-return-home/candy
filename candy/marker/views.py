ba0094abdc016886ccb1789affd7a462b471dd71
imddoy <imddoy@duksung.ac.kr>
2023-11-12 23:10:11 +0900

    Feat: road 페이지

--- candy/marker/views.py
+++ candy/marker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 import requests
 from .models import Marker, Comment
+from django.core.paginator import Paginator
 from django.utils import timezone
 
 def marker_view(request):
@@ -16,7 +17,12 @@ def marker_view(request):
             latitude__range=(latitude - 0.002, latitude + 0.002),
             longitude__range=(longitude - 0.002, longitude + 0.002)
         )
-        return render(request, 'marker/marker.html', {"markers": filtered_markers, "status": status})
+
+        paginator = Paginator(filtered_markers, 3)  # 한 페이지당 3개의 마커 표시
+        page_number = request.GET.get('page')  # 현재 페이지 번호 가져오기
+        page_markers = paginator.get_page(page_number)  # 해당 페이지에 해당하는 마커 가져오기
+
+        return render(request, 'marker/marker.html', {"markers": page_markers, "status": status})
 
 
 # 지도 상세
