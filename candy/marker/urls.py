6e938bf98e467b0566d4d8849e0ff3d03c3b006e
gardening-y <60008786@naver.com>
2023-11-17 22:10:11 +0900

    Fix: marker detail 오류 해결

--- candy/marker/urls.py
+++ candy/marker/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import marker_view, marker_edit_view, marker_detail_view, marker_police_view
+from .views import marker_view, marker_edit_view, marker_detail_view, marker_police_view, marker_detail_delete, marker_detail_update_page,marker_detail_update
 
 app_name = "marker"
 
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', marker_view, name="markers"),
     path('<int:pk>/', marker_detail_view, name="marker_detail"),
     path('edit/', marker_edit_view, name="marker_edit"),
-    path('police/', marker_police_view, name="police")
+    # path('police/', marker_police_view, name="police"),
+    path('delete/<int:pk>/', marker_detail_delete, name="detail_delete"),
+    path('update_page/<int:pk>/', marker_detail_update_page, name="detail_update_page"),
+    path('update/<int:pk>/', marker_detail_update, name="detail_update"),
 ]
